3834c0d285d239847645fbd0ca1f826f02f17757
VisakhSJ <Visakh.S.J@gds.ey.com>
2025-04-23 14:07:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("userForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    document.getElementById("successMessage").classList.remove("hidden");
+  });
+  
